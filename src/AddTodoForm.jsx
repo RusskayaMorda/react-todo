dc067b6dcd1013a7120e5fb095b46e1723e42b62
RusskayaMorda <dimq93@gmail.com>
2024-11-19 19:11:07 -0500

    Added info as per lesson3

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,13 +1,25 @@
-const AddTodoForm = () =>{
+import PropTypes from 'prop-types'
+
+const AddTodoForm = (props) =>{
+    const handleAddTodo = (event) => {
+        event.preventDefault();
+        const todoTitle = event.target.elements.title.value;
+        props.onAddTodo(todoTitle);
+        event.target.reset();
+    }
     return(
         <>
-            <form>
+            <form onSubmit={handleAddTodo}>
                 <label htmlFor="TodoTitle">Title</label>
-                <input type="text" id="TodoTitle" />
+                <input type="text" id="TodoTitle" name="title"/>
                 <button type="submit">Add</button>
             </form>
         </>
     )
 }
+
+AddTodoForm.propTypes = {
+    onAddTodo: PropTypes.func.isRequired,
+};
 
 export default AddTodoForm;
